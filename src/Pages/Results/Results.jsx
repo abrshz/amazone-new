e9741b0b0bc1a2b3ba5354e0a917085bb363ab91
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-02-15 23:06:05 +0300

    Catergory routing done!

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import classes from "./Results.module.css"
 import LayOut from '../../Components/LayOut/LayOut'
+import { useParams } from 'react-router'
+import axios from 'axios'
+import ProductCard from '../../Components/Product/ProductCard'
+import productUrl from "../../Api/endPoints"
 
 function Results() {
+  const [result , setResult] = useState([])
+  const {categoryName} = useParams()
+  useEffect(()=> {
+    axios.get(`${productUrl}/products/category/${categoryName}`)
+    .then((res)=>{
+      setResult(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+      })
+  }, [])
+ 
   return (
     <LayOut>
-      <div>Results</div>
+      <section>
+        <h1 style={{padding: "30px"}}>Results</h1>
+        <p style={{padding: "30px"}}>Category / {categoryName}</p>
+        <div className={classes.products_container}>
+          {result?.map((product)=> (
+            <ProductCard key={product._id} product={product} />
+          )
+          )}
+        </div>
+      </section>
     </LayOut>
   )
 }
